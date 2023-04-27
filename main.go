6988dacb11decdb55cd80fdafe274cae78c972b6
Samuel P <spinillos@wawand.co>
2023-04-27 10:24:01 -0500

    med example

--- main.go
+++ main.go
@@ -1,20 +1,22 @@
 package main
 
 import (
+	_ "embed"
+
 	"github.com/go-rod/rod"
 )
 
-const (
-	url1 = "https://www.google.com"
-	url2 = "https://wawand.co/"
-)
+//go:embed template.html
+var html string
 
 func main() {
 	page := rod.New(). // Creates a new rod instance.
 				MustConnect(). // Connects to the browser.
-				MustPage(url1) // Creates a new page and navigate to it.
+				MustPage()     // Creates a new page and navigate to it. Since no URL is provided, it will navigate to "about:blank".
+
+	page.SetDocumentContent(html) // Sets the page content.
 
 	page.MustWaitLoad() // Waits for the page to load.
 
-	page.MustPDF("google.pdf") // Saves the page as a PDF.
+	page.MustPDF("test.pdf") // Saves the page as a PDF.
 }
