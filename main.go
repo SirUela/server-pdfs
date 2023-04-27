6dea980846875ba05ef7a90c3d6f916956d21e1e
Samuel P <spinillos@wawand.co>
2023-04-27 09:50:28 -0500

    basic go rod example

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/go-rod/rod"
+)
+
+const (
+	url1 = "https://www.google.com"
+	url2 = "https://wawand.co/"
+)
+
+func main() {
+	page := rod.New(). // Creates a new rod instance.
+				MustConnect(). // Connects to the browser.
+				MustPage(url1) // Creates a new page and navigate to it.
+
+	page.MustWaitLoad() // Waits for the page to load.
+
+	page.MustPDF("google.pdf") // Saves the page as a PDF.
+}
