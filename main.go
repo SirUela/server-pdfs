3c1995677f8e6ec279736c55a76e1abdbdec2b0f
Samuel P <spinillos@wawand.co>
2023-04-27 10:40:11 -0500

    advance

--- main.go
+++ main.go
@@ -1,22 +1,92 @@
 package main
 
 import (
+	"bytes"
 	_ "embed"
+	"fmt"
+	"io"
 
 	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
+	"github.com/gobuffalo/buffalo"
+	"github.com/gobuffalo/buffalo/render"
 )
 
-//go:embed template.html
-var html string
+var (
+	A4Width  = 8.5
+	A4Height = 11.0
 
-func main() {
-	page := rod.New(). // Creates a new rod instance.
-				MustConnect(). // Connects to the browser.
-				MustPage()     // Creates a new page and navigate to it. Since no URL is provided, it will navigate to "about:blank".
+	//go:embed template.html
+	html string
 
-	page.SetDocumentContent(html) // Sets the page content.
+	r = render.New(render.Options{})
+)
 
-	page.MustWaitLoad() // Waits for the page to load.
+func SomeEndpoint(c buffalo.Context) {
+	data := map[string]interface{}{
+		"random": "data",
+		"more":   "data",
+	}
 
-	page.MustPDF("test.pdf") // Saves the page as a PDF.
+	c.Set("needed", "data")
+	c.Set("needed2", "data2")
+
+	data2 := c.Data()
+	fmt.Println(data2)
+
+	html := &bytes.Buffer{}
+	if err := r.HTML("/invoices/pdf.plush.html", "reports.plush.html").Render(html, data); err != nil {
+		// handle error
+	}
+
+	file, err := Render(html.String())
+	if err != nil {
+		// handle error
+	}
+
+	c.Response().Header().Set("Content-Disposition", "attachment; filename=report.pdf")
+	c.Response().Header().Set("Content-Type", "application/pdf")
+
+	// do something with the file
+	fmt.Println(file)
+}
+
+func Render(html string) (*bytes.Buffer, error) {
+	u := launcher.New().
+		Headless(true).
+		MustLaunch()
+
+	page := rod.New().ControlURL(u).Trace(true).MustConnect().MustPage("about:blank")
+
+	err := page.MustWaitLoad().SetDocumentContent(html)
+	if err != nil {
+		return nil, fmt.Errorf("error setting document content: %w", err)
+	}
+	page.MustWaitLoad()
+
+	margin := 0.0
+	settings := &proto.PagePrintToPDF{
+		PrintBackground: true,
+		PaperWidth:      &A4Width,
+		PaperHeight:     &A4Height,
+		MarginTop:       &margin,
+		MarginBottom:    &margin,
+		MarginLeft:      &margin,
+		MarginRight:     &margin,
+	}
+
+	pdf, err := page.PDF(settings)
+	if err != nil {
+		return nil, fmt.Errorf("error rendering PDF: %w", err)
+	}
+	defer pdf.Close()
+
+	file := &bytes.Buffer{}
+	_, err = io.Copy(file, pdf)
+	if err != nil {
+		return nil, fmt.Errorf("error copying PDF: %w", err)
+	}
+
+	return file, nil
 }
